d13cb841314f0a3d8a8facb044d684d600119cbe
AD Laksito <me@ariflaksito.net>
2020-10-02 09:42:29 +0700

    Add project day4 - OptionMenu

---
+++ App04b/app/src/main/java/net/ariflaksito/app04b/SettingActivity.java
@@ -0,0 +1,14 @@
+package net.ariflaksito.app04b;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
