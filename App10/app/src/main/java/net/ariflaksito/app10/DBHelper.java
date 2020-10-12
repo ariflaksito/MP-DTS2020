c2b2eb2ddd57e0fc5f7f99b88186b6a2aaddd029
AD Laksito <me@ariflaksito.net>
2020-10-12 08:43:12 +0700

    Add project day 10 - SQLite DB

---
+++ App10/app/src/main/java/net/ariflaksito/app10/DBHelper.java
@@ -0,0 +1,61 @@
+package net.ariflaksito.app10;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    static String DBNAME = "studentdb";
+    static final int DBVER = 1;
+    static String TB_STUDENT = "students";
+    static String KEY_ID = "id";
+    static String KEY_NAME = "name";
+
+    public DBHelper(Context context) {
+        super(context, DBNAME, null, DBVER);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTable = "CREATE TABLE "+ TB_STUDENT +" ("+
+                KEY_ID+" INTEGER PRIMARY KEY AUTOINCREMENT," +
+                KEY_NAME+" TEXT)";
+        db.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String dropTable = "DROP TABLE IF EXISTS '"+ TB_STUDENT +"'";
+        onCreate(db);
+    }
+
+    public long addStudent(String name){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(KEY_NAME, name);
+
+        long insert = db.insert(TB_STUDENT, null, cv);
+        return insert;
+    }
+
+    public ArrayList<String> getStudentList(){
+        ArrayList<String> listStudent = new ArrayList<String>();
+        String querySelect = "SELECT id, name FROM "+TB_STUDENT;
+
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor c = db.rawQuery(querySelect, null);
+        if(c.moveToFirst()){
+            do{
+                String name = c.getString(c.getColumnIndex(KEY_NAME));
+                listStudent.add(name);
+            }while (c.moveToNext());
+        }
+
+        return listStudent;
+    }
+}
---
+++ App10/app/src/main/java/net/ariflaksito/app10/MainActivity.java
@@ -0,0 +1,60 @@
+package net.ariflaksito.app10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    DBHelper db;
+    EditText inName;
+    Button btnAdd, btnShow;
+    ArrayList<String> listData;
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        db = new DBHelper(this);
+        inName = findViewById(R.id.in_name);
+        btnAdd = findViewById(R.id.btn_add);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                db.addStudent(inName.getText().toString());
+                inName.setText("");
+                Toast.makeText(MainActivity.this, "Input name success", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnShow = findViewById(R.id.btn_show);
+        btnShow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listData = db.getStudentList();
+                inName.setText("");
+                showData();
+            }
+        });
+
+        listView = findViewById(R.id.listview);
+
+    }
+
+    void showData(){
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, listData);
+
+        listView.setAdapter(adapter);
+    }
+}
